3c5c23610c121a6b8a7cbc7c87ed86904ea7c091
Will <sypheren@gmail.com>
2023-06-02 08:55:11 -0500

    test

--- docs/make.jl
+++ docs/make.jl
@@ -4,3 +4,8 @@ makedocs(sitename="MembraneBase.jl",
     format = Documenter.HTML(
         prettyurls = get(ENV, "CI", nothing) == "true"
     ))
+
+
+deploydocs(
+    repo = "github.com/Boxylmer/MembraneBase.jl.git",
+)
