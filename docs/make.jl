ae3fbd6e8a2cb2a3d9ff978bb5fc3944e6cf072d
Will <sypheren@gmail.com>
2023-07-08 01:00:03 -0500

    push to separte repo attempt 1

--- docs/make.jl
+++ docs/make.jl
@@ -1,11 +1,24 @@
 using Documenter, MembraneBase
 
-makedocs(sitename="MembraneBase.jl",
-    format = Documenter.HTML(
-        prettyurls = get(ENV, "CI", nothing) == "true"
-    ))
+DocMeta.setdocmeta!(MembraneBase, :DocTestSetup, :(using MembraneBase); recursive=true)
 
+makedocs(;
+    modules=[MembraneBase],
+    authors="Will <sypheren@gmail.com> and contributors",
+    repo="https://github.com/Boxylmer/MembraneBase.jl/blob/{commit}{path}#{line}",
+    sitename="MembraneBase.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://Boxylmer.github.io/MembraneBase.jl",
+        edit_link="master",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+)
 
-deploydocs(
-    repo = "github.com/Boxylmer/MembraneBase.jl.git",
+deploydocs(;
+    repo="github.com/Boxylmer/MembraneBaseDocs",
+    devbranch="master",
 )
