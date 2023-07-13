057e112527f7ee82d6561d406116b231ad55beba
Will <carboxylmer@gmail.com>
2023-07-13 15:12:59 -0500

    more commit

--- registry/register.jl
+++ registry/register.jl
@@ -1,4 +1,3 @@
 using LocalRegistry
 using MembraneBase
-register(; branch="master")
-#registry="github.com/Boxylmer/MembraneRegistry"
+register(; branch="master", registry="github.com/Boxylmer/MembraneRegistry"
